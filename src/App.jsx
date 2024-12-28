31a9f75446e89c0b94900c0b29a34994858fd97e
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-12-29 01:45:39 +0530

    Hero "Work" section started

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react"
 import Navbar from "./components/Navbar"
+import Work from "./components/Work"
 
 const App = () => {
   return (
     <div className='w-full h-screen bg-zinc-900 font-["satoshi"] text-white'>
       <Navbar />
+      <Work />
     </div>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,8 +5,8 @@ import { IoIosReturnRight } from "react-icons/io"
 
 const Button = ({ title = "Get Started" }) => {
   return (
-    <div className="w-[130px] px-4 py-2 bg-zinc-100 text-black rounded-full flex items-center justify-between">
-      <span className="text-sm font-medium">{title}</span>
+    <div className='w-[130px] px-4 py-2 bg-zinc-100 text-black rounded-full flex items-center justify-between'>
+      <span className='text-sm font-medium'>{title}</span>
       <IoIosReturnRight />
     </div>
   )
