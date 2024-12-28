44a3307f348d2ae25c8cdf3302162101b4e02093
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-12-29 01:35:48 +0530

    Navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react"
+import Navbar from "./components/Navbar"
 
 const App = () => {
-  return <div className="w-full h-screen bg-zinc-900">App</div>
+  return (
+    <div className='w-full h-screen bg-zinc-900 font-["satoshi"] text-white'>
+      <Navbar />
+    </div>
+  )
 }
 
 export default App
