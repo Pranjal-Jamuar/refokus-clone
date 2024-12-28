0ccd705589940a875c1ef5af18aa41a5bc1fe688
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-12-29 02:59:07 +0530

    Products Section completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Work from "./components/Work"
 import Stripes from "./components/Stripes/Stripes"
+import Products from "./components/Products/Products"
 
 const App = () => {
   return (
-    <div className='w-full h-screen bg-zinc-900 font-["satoshi"] text-white'>
+    <div className='w-full bg-zinc-900 font-["satoshi"] text-white'>
       <Navbar />
       <Work />
       <Stripes />
+      <Products />
     </div>
   )
 }
