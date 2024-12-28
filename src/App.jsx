0b848e020679ab66e167cabaf1c3a02433ce9590
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-12-29 04:27:53 +0530

    Animation part started Added smooth scrolling usinf locomotive-scroll

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ import Products from "./components/Products/Products"
 import Marquees from "./components/Marquees/Marquees"
 import Cards from "./components/Cards/Cards"
 import Footer from "./components/Footer"
+import LocomotiveScroll from "locomotive-scroll"
 
 const App = () => {
+  const locomotiveScroll = new LocomotiveScroll()
+
   return (
     <div className='w-full bg-zinc-900 font-["satoshi"] text-white'>
       <Navbar />
