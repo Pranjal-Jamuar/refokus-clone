f1aaeaf8d00292d3fc8895a436a7f7e46dacf7bf
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-12-29 04:58:11 +0530

    Products component animation started

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import Product from "./Product"
+import { motion } from "motion/react"
 
 import arqitel from "../../../public/videos/arqitel.webm"
 import ttr from "../../../public/videos/ttr.webm"
