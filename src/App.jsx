f8ee8174f35b03e4e2cd851075373b38d6def253
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-12-29 01:58:55 +0530

    Stripe Section Started

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Work from "./components/Work"
+import Stripes from "./components/Stripes/Stripes"
 
 const App = () => {
   return (
     <div className='w-full h-screen bg-zinc-900 font-["satoshi"] text-white'>
       <Navbar />
       <Work />
+      <Stripes />
     </div>
   )
 }
---
+++ src/components/Stripes/Stripes.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react"
+import Stripe from "./Stripe"
+
+const Stripes = () => {
+  return (
+    <div>
+      <Stripe />
+    </div>
+  )
+}
+
+export default Stripes
