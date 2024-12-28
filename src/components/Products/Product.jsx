418e11c688a5e0980e996e452717b5129dd7772c
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-12-29 05:19:29 +0530

    Products Component animation completed

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -5,7 +5,7 @@ const Product = ({ val, count, mover }) => {
   return (
     <div className='w-full h-[20rem] py-20 '>
       <div
-        onmouseenter={() => mover(count)}
+        onMouseEnter={() => mover(count)}
         className='max-w-screen-xl mx-auto flex items-center justify-between'
       >
         <h1 className='text-6xl capitalize font-medium'>{val.title}</h1>
